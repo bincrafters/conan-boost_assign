dacaf8c8248653054f5ac62fa81216e2afefe191
Jerry W <jerrywiltse@gmail.com>
2018-02-02 07:08:36 -0500

    Merge testing into stable

--- conanfile.py
+++ conanfile.py
@@ -17,16 +17,16 @@ class BoostAssignConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing",
-        "boost_array/1.66.0@bincrafters/testing",
-        "boost_config/1.66.0@bincrafters/testing",
-        "boost_mpl/1.66.0@bincrafters/testing",
-        "boost_preprocessor/1.66.0@bincrafters/testing",
-        "boost_ptr_container/1.66.0@bincrafters/testing",
-        "boost_range/1.66.0@bincrafters/testing",
-        "boost_static_assert/1.66.0@bincrafters/testing",
-        "boost_tuple/1.66.0@bincrafters/testing",
-        "boost_type_traits/1.66.0@bincrafters/testing"
+        "boost_package_tools/1.66.0@bincrafters/stable",
+        "boost_array/1.66.0@bincrafters/stable",
+        "boost_config/1.66.0@bincrafters/stable",
+        "boost_mpl/1.66.0@bincrafters/stable",
+        "boost_preprocessor/1.66.0@bincrafters/stable",
+        "boost_ptr_container/1.66.0@bincrafters/stable",
+        "boost_range/1.66.0@bincrafters/stable",
+        "boost_static_assert/1.66.0@bincrafters/stable",
+        "boost_tuple/1.66.0@bincrafters/stable",
+        "boost_type_traits/1.66.0@bincrafters/stable"
     )
 
     # BEGIN
@@ -34,7 +34,7 @@ class BoostAssignConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.66.0@bincrafters/stable"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
